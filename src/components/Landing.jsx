f31fd70714060db379ce3601a867d93dad69725a
AryalSubin <aryalsuubin@gmail.com>
2024-02-19 12:05:40 +0545

    first commit and added navbar

---
+++ src/components/Landing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <div className="bg-zinc-900 text-white w-full h-screen">
+      <div className="text-structure">
+        <div className="masker">
+          <h1 className="uppercase">we create</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex fixed top-0 left-0 w-full text-gray-100 shadow-sm bg-transparent text-md  font-poppins p-2 justify-between ">
+      <div className="flex sm:flex-[.2] md:flex-[.5] lg:flex-[.7] xl:flex-1 items-center">
+        <span className="px-6   font-bold  ">Animuse</span>
+      </div>
+      <div className="sm:flex hidden  font-light flex-1 capitalize p-1 gap-8">
+        {["services", "our work", "about us", "insights", "contact us"].map(
+          (item, id) => {
+            return id === 4 ? (
+              <div className="relative ml-[15vw]">
+                <a
+                  // style={{
+                  //     // transform: "translateY(20px)",
+                  //   transition:
+                  //     // "transform 0.5s cubic-bezier(0.2, 0.6, 0.35, 1) 0s",
+                  // }}
+                  className="navbtn   cursor-pointer "
+                  key={id}
+                >
+                  {item}
+                </a>
+              </div>
+            ) : (
+              <div className="relative">
+                <a
+                  //   style={{
+                  //     //   transform: "translateY(20px)",
+                  //     transition:
+                  //       "transform 0.5s cubic-bezier(0.2, 0.6, 0.35, 1) 0s",
+                  //   }}
+                  className="relative navbtn cursor-pointer  hover:transition-transform  transition-[2]"
+                  key={id}
+                >
+                  {item}
+                </a>
+              </div>
+            );
+          }
+        )}
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
